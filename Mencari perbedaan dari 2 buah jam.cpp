23f4323dfb2e2c2b19394c4f38db7b73e9c6f1e5
Mida Adawiyah <midaadawiyah0927@gmail.com>
2020-12-09 09:50:43 +0700

    Tugas6Algo

---
+++ Mencari perbedaan dari 2 buah jam.cpp
@@ -0,0 +1,28 @@
+//mencari perbedaan dari 2 buah jam (program billing telepon / warnet)
+//deklarasi:
+Bilangan Bulat j1,m1,d1,td1,j2,m2,d2,td2,j3,m3,d3,td3
+deskripsi program:
+	
+//masukan waktu awal ex	9:40:10
+input(j1)
+input(m1)
+input(d1)
+
+//masukan waktu akhir ex 10:00:00
+input(j2)
+input(m2)
+input(d2)
+
+///proses konversi waktu ke td
+td1=konversi(j1,m1,d1)
+td2=konversi(j2,m2,d2)
+
+//operasi pengurangan
+td3=td2-td1
+
+//konversi td ke j3, m3, d3
+j3,m3,d3 => (td3)
+
+//tampilan layar ex 1:2:5
+output(j3,m3,d3)
+
